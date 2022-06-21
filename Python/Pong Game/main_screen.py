35bb21167c1535b5a0c607bb2999aad9df252e7f
Ivan <shouravivan@gmail.com>
2022-06-21 11:48:03 -0600

    Added the difficulty feature.
    The difficulty level of the game increases after every round.

--- Python/Pong Game/main_screen.py
+++ Python/Pong Game/main_screen.py
@@ -24,10 +24,23 @@ screen.onkey(player_two.move_up, "w")
 screen.onkey(player_two.move_down, "s")
 
 game_on = True
-
+difficult_level = .10
+# The game is played ten rounds. The one who wins the most rounds will win the game.
+# The pace of the ball increases after every round.
 while game_on:
     screen.update()
-    time.sleep(.09)
+    if difficult_level < .01:
+        if score.player_one_score > score.player_two_score:
+            print(f"The winner is player one with {score.player_one_score} points")
+            game_on = False
+        elif score.player_two_score > score.player_one_score:
+            print(f"The winner is player two with {score.player_two_score} points")
+            game_on = False
+        else:
+            print("The game is draw")
+            game_on = False
+    time.sleep(difficult_level)
+
     game_ball.move()
 
     # If it collides with the wall, it will just bounce the ball back.
@@ -46,11 +59,12 @@ while game_on:
     elif game_ball.pong_ball.xcor() > 450:
         score.paddle_one_score()
         game_ball.reset_ball()
+        difficult_level = difficult_level - .01
 
     # If the x coordinate of the ball is lesser than -450, than the first paddle will gain one point.
     elif game_ball.pong_ball.xcor() < -450:
         score.paddle_two_score()
         game_ball.reset_ball()
-
+        difficult_level = difficult_level - .01
 
 screen.exitonclick()
