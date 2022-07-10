bf366ed8fa29db80ada7245d594fedd7f6f78ba2
Ivan <shouravivan@gmail.com>
2022-07-10 03:16:33 -0600

    Created a Python GUI Password Manager
    You can easily generate strong random password with this app.

--- Python/Password Manager GUI/main.py
+++ Python/Password Manager GUI/main.py
@@ -1,19 +1,39 @@
 from tkinter import *
+from tkinter import messagebox
 import random
+import string
 
 
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# This function generates random password for the users if they click the generate password button.
+def generate_password():
+    alphabet = list(string.ascii_letters)
+    symbols = ['~', ':', "'", '+', '[', '\\', '@', '^', '{', '%', '(', '-', '"', '*', '|', ',', '&', '<', '`', '}', '.',
+               '_', '=', ']', '!', '>', ';', '?', '#', '$', ')', '/']
+    numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    strong_password_list = random.sample(alphabet, random.randint(1, 4)) + random.sample(symbols,
+                                                                                         random.randint(1, 4)) + \
+                           random.sample(numbers, random.randint(1, 4))
+    strong_password = ""
+    for key in range(len(strong_password_list)):
+        strong_password = strong_password + str(strong_password_list[key])
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+    password_entry.insert(0, strong_password)
+
+
 def add_info():
-    with open("data.txt", mode="a") as f:
-        website = f"{website_entry.get()} | "
-        email = f"{email_entry.get()} | "
-        password = f"{password_entry.get()} \n"
-
-        store_content = f"{f.write(website)} {f.write(email)} {f.write(password)}"
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    if len(website) < 2 or len(password) < 2:
+        messagebox.showinfo(title="Oops", message="Please don't leave any fields empty!")
+    else:
+        details = messagebox.askokcancel(title=website,
+                                         message=f"These are details you have entered\n Email: {email}\n Password: {password}")
+        if details:
+            with open("data.txt", mode="a") as f:
+                store_content = f.write(f"{website} | {email} | {password}\n")
+                website_entry.delete(0, END)
+                password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -57,7 +77,7 @@ password_entry = Entry(width=21)
 password_entry.place(x=100, y=264)
 
 # Password generate button
-button = Button(width=14, text="Generate Password")
+button = Button(width=14, text="Generate Password", command=generate_password)
 button.place(x=235, y=260)
 
 add_button = Button(width=33, text="Add", command=add_info)
