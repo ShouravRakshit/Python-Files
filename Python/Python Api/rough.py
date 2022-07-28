84db91106082c93be6ec009cc326da8f0e8c8f0b
Ivan <shouravivan@gmail.com>
2022-07-27 21:17:07 -0600

    Update rough.py

--- Python/Python Api/rough.py
+++ Python/Python Api/rough.py
@@ -1,18 +1,1 @@
-import os
-# from twilio.rest import Client
-#
-#
-# # Find your Account SID and Auth Token at twilio.com/console
-# # and set the environment variables. See http://twil.io/secure
-# account_sid = "ACae6de17f94e215a49e969221a2ffe770"
-# auth_token = "b48a06587614ec795710cf121e114a88"
-# client = Client(account_sid, auth_token)
-#
-# message = client.messages.create(
-#                               from_='+19105972698',
-#                               body='Hi there Boi!',
-#                               to='+18253652839'
-#                           )
-#
-# print(message.sid)
 
