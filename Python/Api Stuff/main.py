304aec4172d29a58d609c5a222818c410dba495e
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2023-06-05 21:07:43 -0600

    Update main.py

--- Python/Api Stuff/main.py
+++ Python/Api Stuff/main.py
@@ -1,4 +1,17 @@
-#Guess the output of each print statement before you click RUN!
-python = 'I am PYHTON'
+# using this list,
+basket = ["Banana", "Apples", "Oranges", "Blueberries"];
+# 1. Remove the Banana from the list
+basket.remove("Banana")
 
-print(python[1:100])
+# 2. Remove "Blueberries" from the list.
+basket.remove("Blueberries")
+
+# 3. Put "Kiwi" at the end of the list.
+basket.append("Kiwi")
+# 4. Add "Apples" at the beginning of the list
+
+# 5. Count how many apples in the basket
+
+# 6. empty the basket
+
+print(basket)
