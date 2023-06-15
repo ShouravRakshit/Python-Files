c969ce9774d8e470939a7c7390d638aabae0f13a
Shourav Rakshit Ivan <69850843+ShouravRakshit@users.noreply.github.com>
2023-06-14 23:36:11 -0600

    Working on a JPG to PNG converter

--- Python/Api Stuff/testing.py
+++ Python/Api Stuff/testing.py
@@ -1,41 +1,57 @@
-# from googletrans import Translator
-#
-# f = open("test1.txt", "r")
-# text = (f.readlines())
-# new_text = ""
-# for letters in text:
-#     new_text = new_text + letters
-#
-# translator = Translator()
-# print(translator.translate(new_text, dest='ja'))
-# print("This is a comment")
-#
-# f.close()
-
-# from random import randint
-# import sys
-#
-# # generate a number 1~10
-# answer = randint(1, 10)
-#
-# # input from user?
-# # check that input is a number 1~10
-# while True:
-#     try:
-#         guess = int(input('guess a number 1~10:  '))
-#         if 0 < guess < 11:
-#             if guess == answer:
-#                 print('you are a genius!')
-#                 break
-#         else:
-#             print('hey bozo, I said 1~10')
-#     except ValueError:
-#         print('please enter a number')
-
 from PIL import Image, ImageFilter
+import sys
+import os
 
-im = Image.open("209 - pikachu.jpg")
-filtered_img = im.filter(ImageFilter.BLUR)
-print(im.format, im.size, im.mode)
+# Check if command-line arguments were provided
+if len(sys.argv) > 1:
+    # Access the arguments starting from index 1
+    # as the index 0 is the script name
+    arguments = sys.argv[1:]
 
+    # Process the arguments
+    current_directory = os.path.dirname(os.path.abspath(__file__))
 
+    # Specify the name of the new folder
+    try:
+        folder_name = f"{arguments[0]}"
+
+    # Create the folder path by joining the current directory with the folder name
+        folder_path = os.path.join(current_directory, folder_name)
+        print("Bal")
+        os.mkdir(folder_path)
+        print("meh")
+    except FileExistsError:
+        print("Folder already exits.")
+
+    # Specify the path and filename of the input JPG image
+    # input_folder_path = "./Pokedex"
+    # output_folder_path = "./new_pics"
+
+    input_folder_path = f"{arguments[0]}"
+    output_folder_path = f"{arguments[1]}"
+
+    # Loop through all files in the folder
+    for filename in os.listdir(input_folder_path):
+        # Construct the full file path
+        input_file_path = os.path.join(input_folder_path, filename)
+        # string = f"{filename}"
+        # substring = ".jpg"
+        #
+        # filename = string.rstrip(substring)
+
+        output_file_path = os.path.join(output_folder_path, filename)
+
+        input_image_path = f"{input_file_path}"
+
+        # Specify the path and filename for the output PNG image
+        # output_image_path = f"{output_file_path}"
+        output_image_path = os.path.splitext(output_file_path)[0] + ".png"
+
+        # Open the input image
+        image = Image.open(input_image_path)
+
+        # Convert the image to PNG format
+        image.save(output_image_path, "png")
+
+else:
+    print("No command-line arguments provided.")
